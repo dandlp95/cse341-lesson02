827c20f75b21950e050b550894df8441b7b53ebf
dandlp95 <del17011@byui.edu>
2022-04-30 19:59:29 -0600

    finished

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,12 +1,27 @@
 const routes = require('express').Router();
 const connect = require('../db/connect');
+const ObjectId = require('mongodb').ObjectId;
 
 routes.get('/', function (req, res){
 
-    connect.getCollection().find().toArray((err, result)=>{
-            if (err) throw err;
-            res.json(result);
-            console.log('Contacts Query Succesful')
+    const results = connect.getCollection().find();
+
+    results.toArray().then((documents)=>{
+            res.status(200).json(documents);
+            console.log('Returned all contacts.');
         });
 });
+
+routes.get('/:id', function (req, res){
+
+    const contactId = new ObjectId(req.params.id);
+    const results = connect.getCollection().find({_id: contactId});
+
+    results.toArray().then((documents)=>{
+            res.status(200).json(documents[0]);
+            console.log('Returned one contact.');
+        });
+});
+
+
 module.exports = routes;
