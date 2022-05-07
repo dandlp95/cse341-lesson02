a3ae6d32eb459d2bfef7e38b0ad50afab647ab2a
dandlp95 <del17011@byui.edu>
2022-05-07 04:42:34 -0600

    finished

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,27 +1,69 @@
 const routes = require('express').Router();
-const connect = require('../db/connect');
+const contactModel = require('../db/model')
 const ObjectId = require('mongodb').ObjectId;
 
-routes.get('/', function (req, res){
+routes.get('/', async function (req, res){
 
-    const results = connect.getCollection().find();
-
-    results.toArray().then((documents)=>{
-            res.status(200).json(documents);
-            console.log('Returned all contacts.');
-        });
+    const contacts = await contactModel.find({});
+  
+    try {
+      res.status(200).send(contacts);
+    } catch (error) {
+      res.status(500).send(error);
+    }
 });
 
-routes.get('/:id', function (req, res){
+routes.get('/:id', async function (req, res){
 
-    const contactId = new ObjectId(req.params.id);
-    const results = connect.getCollection().find({_id: contactId});
-
-    results.toArray().then((documents)=>{
-            res.status(200).json(documents[0]);
-            console.log('Returned one contact.');
-        });
+   const contact = await contactModel.findById(req.params.id);
+   try{
+       res.status(200).send(contact);
+   }catch (error) {
+       res.status(500).send(error);
+   }
 });
+
+routes.post('/add_contacts', async function (req, res){
+
+    const contact = new contactModel(req.body);
+
+    try{
+        await contact.save();
+        res.status(200).send(contact);
+    }catch (error) {
+        res.status(500).send(error);
+    }
+})
+
+routes.put('/edit_contact/:id', function (req, res){
+
+    contactModel.findByIdAndUpdate(req.params.id, req.body, function(err, docs){
+        if(err){
+            res.status(400).send(err);
+        }else{
+            res.status(200).send(docs);
+        }
+    })
+    
+})
+
+routes.delete('/delete/:id', function (req, res){
+
+    contactModel.findByIdAndDelete(req.params.id, (err, docs)=>{
+        if (err) {
+            res.status(400).send(err);
+        }
+        else {
+            if (docs === null) {
+                res.status(200).send("Already Deleted");
+            }
+            else {
+                res.status(200).send(docs)
+                res.send(docs);
+            }}
+    })
+})
 
 
 module.exports = routes;
+
