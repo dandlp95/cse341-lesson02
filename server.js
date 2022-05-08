a53c2be93c4a12b8ce2a23ed67dae04d623868e6
dandlp95 <del17011@byui.edu>
2022-05-07 22:09:54 -0600

    cors

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const PORT = process.env.PORT || 3000;
 const app = express();
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 // const connection = require('./db/connect');
 
@@ -12,8 +13,10 @@ mongoose
     .connect(process.env.MONGODB_URL)
     .then(() =>{
         
-        app.use(express.json());
-        app.use('/', require('./routes'));
+        app
+            .use(express.json())
+            .use(cors)
+            .use('/', require('./routes'));
 
         app.listen(PORT, ()=>{
         console.log(`listening on PORT ${PORT}`);
